20e839d331158622fcdac131dfb843b774dc6227
Yacoub BELAOURA <yacoubbelaoura@gmail.com>
2022-08-27 12:13:18 +0100

    :bento: add spacevim autoload conf for Coc ext

--- update_repo.sh
+++ update_repo.sh
@@ -2,5 +2,6 @@
 rsync -raczv ~/.config/polybar .
 rsync -raczv ~/.config/ranger .
 rsync -raczv ~/.config/i3 .
+rsync -raczv ~/.SpaceVim.d .
 rsync -raczv ~/.zshrc .
 rsync -raczv ~/.zshrc-personal .
